9e9efe56e6138f65105441b36d5a87fcccc8af43
thanhrhy1 <85609412+thanhrhy1@users.noreply.github.com>
2021-06-10 18:05:46 +0700

    Calculator

---
+++ btapCalculator_TTNT(coHa)_nhom12/btapCalculator_TTNT(coHa)_nhom12/Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿
+namespace btapCalculator_TTNT_coHa__nhom12
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bntGiatri1 = new System.Windows.Forms.Label();
+            this.bntGiatri2 = new System.Windows.Forms.Label();
+            this.bntKetqua = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.bntPhepcong = new System.Windows.Forms.Button();
+            this.bntPheptru = new System.Windows.Forms.Button();
+            this.bntPhepnhan = new System.Windows.Forms.Button();
+            this.bntPhepchia = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // bntGiatri1
+            // 
+            this.bntGiatri1.AutoSize = true;
+            this.bntGiatri1.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bntGiatri1.Location = new System.Drawing.Point(23, 24);
+            this.bntGiatri1.Name = "bntGiatri1";
+            this.bntGiatri1.Size = new System.Drawing.Size(79, 22);
+            this.bntGiatri1.TabIndex = 0;
+            this.bntGiatri1.Text = "Giá trị 1";
+            // 
+            // bntGiatri2
+            // 
+            this.bntGiatri2.AutoSize = true;
+            this.bntGiatri2.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bntGiatri2.Location = new System.Drawing.Point(23, 83);
+            this.bntGiatri2.Name = "bntGiatri2";
+            this.bntGiatri2.Size = new System.Drawing.Size(79, 22);
+            this.bntGiatri2.TabIndex = 1;
+            this.bntGiatri2.Text = "Giá trị 2";
+            // 
+            // bntKetqua
+            // 
+            this.bntKetqua.AutoSize = true;
+            this.bntKetqua.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bntKetqua.Location = new System.Drawing.Point(23, 134);
+            this.bntKetqua.Name = "bntKetqua";
+            this.bntKetqua.Size = new System.Drawing.Size(74, 22);
+            this.bntKetqua.TabIndex = 2;
+            this.bntKetqua.Text = "Kết quả";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(118, 27);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(118, 85);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(118, 133);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 5;
+            // 
+            // bntPhepcong
+            // 
+            this.bntPhepcong.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bntPhepcong.Location = new System.Drawing.Point(244, 12);
+            this.bntPhepcong.Name = "bntPhepcong";
+            this.bntPhepcong.Size = new System.Drawing.Size(75, 34);
+            this.bntPhepcong.TabIndex = 6;
+            this.bntPhepcong.Text = "+";
+            this.bntPhepcong.UseVisualStyleBackColor = true;
+            this.bntPhepcong.Click += new System.EventHandler(this.bntPhepcong_Click);
+            // 
+            // bntPheptru
+            // 
+            this.bntPheptru.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bntPheptru.Location = new System.Drawing.Point(244, 52);
+            this.bntPheptru.Name = "bntPheptru";
+            this.bntPheptru.Size = new System.Drawing.Size(75, 33);
+            this.bntPheptru.TabIndex = 7;
+            this.bntPheptru.Text = "-";
+            this.bntPheptru.UseVisualStyleBackColor = true;
+            this.bntPheptru.Click += new System.EventHandler(this.bntPheptru_Click);
+            // 
+            // bntPhepnhan
+            // 
+            this.bntPhepnhan.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bntPhepnhan.Location = new System.Drawing.Point(244, 91);
+            this.bntPhepnhan.Name = "bntPhepnhan";
+            this.bntPhepnhan.Size = new System.Drawing.Size(75, 36);
+            this.bntPhepnhan.TabIndex = 8;
+            this.bntPhepnhan.Text = "*";
+            this.bntPhepnhan.UseVisualStyleBackColor = true;
+            this.bntPhepnhan.Click += new System.EventHandler(this.bntPhepnhan_Click);
+            // 
+            // bntPhepchia
+            // 
+            this.bntPhepchia.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bntPhepchia.Location = new System.Drawing.Point(244, 133);
+            this.bntPhepchia.Name = "bntPhepchia";
+            this.bntPhepchia.Size = new System.Drawing.Size(75, 34);
+            this.bntPhepchia.TabIndex = 9;
+            this.bntPhepchia.Text = "/";
+            this.bntPhepchia.UseVisualStyleBackColor = true;
+            this.bntPhepchia.Click += new System.EventHandler(this.bntPhepchia_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(338, 179);
+            this.Controls.Add(this.bntPhepchia);
+            this.Controls.Add(this.bntPhepnhan);
+            this.Controls.Add(this.bntPheptru);
+            this.Controls.Add(this.bntPhepcong);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.bntKetqua);
+            this.Controls.Add(this.bntGiatri2);
+            this.Controls.Add(this.bntGiatri1);
+            this.Name = "Form1";
+            this.Text = "Máy tính thông dụng";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label bntGiatri1;
+        private System.Windows.Forms.Label bntGiatri2;
+        private System.Windows.Forms.Label bntKetqua;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button bntPhepcong;
+        private System.Windows.Forms.Button bntPheptru;
+        private System.Windows.Forms.Button bntPhepnhan;
+        private System.Windows.Forms.Button bntPhepchia;
+    }
+}
+
---
+++ btapCalculator_TTNT(coHa)_nhom12/btapCalculator_TTNT(coHa)_nhom12/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace btapCalculator_TTNT_coHa__nhom12
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void bntPhepcong_Click(object sender, EventArgs e)
+        {
+            double txt1 = Convert.ToDouble(textBox1.Text);
+            double txt2 = Convert.ToDouble(textBox2.Text);
+            double sum = txt1 + txt2;
+            textBox3.Text = sum.ToString();
+        }
+
+        private void bntPheptru_Click(object sender, EventArgs e)
+        {
+            double txt1 = Convert.ToDouble(textBox1.Text);
+            double txt2 = Convert.ToDouble(textBox2.Text);
+            double sum = txt1 - txt2;
+            textBox3.Text = sum.ToString();
+        }
+
+        private void bntPhepnhan_Click(object sender, EventArgs e)
+        {
+            double txt1 = Convert.ToDouble(textBox1.Text);
+            double txt2 = Convert.ToDouble(textBox2.Text);
+            double sum = txt1 * txt2;
+            textBox3.Text = sum.ToString();
+        }
+
+        private void bntPhepchia_Click(object sender, EventArgs e)
+        {
+            double txt1 = Convert.ToDouble(textBox1.Text);
+            double txt2 = Convert.ToDouble(textBox2.Text);
+            double sum = txt1 / txt2;
+            textBox3.Text = sum.ToString();
+        }
+    }
+}
